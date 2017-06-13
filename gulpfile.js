b2c2978480d0db6f66f97e9a09d8f314878cde91
sychovOleh <sychovoleh@gmail.com>
2017-06-13 16:04:58 +0300

    fix gulp autoPrefixer & concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,20 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-const concat = require('gulp-concat')
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('./src/styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 6 versions'],
+      cascade: false
+    }))
     .pipe(concat('main.css'))
-    .pipe(gulp.dest('./dest'));
-});
+    .pipe(gulp.dest('./dest'))
+})
 
 gulp.task('sass:watch', function () {
   gulp.watch('./src/styles/**/*.scss', ['sass']);
 });
-
-// 
-
-// var input = './src/styles/**/*.scss';
-// var output = './main/css';
-
-// gulp.task('sass', function () {
-//   return gulp.src(input)
-//     .pipe(sass())
-//     .pipe(gulp.dest(output));
-// });
-// var sassOptions = {
-//   errLogToConsole: true,
-//   outputStyle: 'expanded'
-// };
-
-// gulp.task('sass', function () {
-//   return gulp
-//     .src(input)
-//     .pipe(sass(sassOptions).on('error', sass.logError))
-//     .pipe(gulp.dest(output));
-// });
