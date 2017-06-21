79aac21f05b5e616b7722e19c04b94bf749d7cb0
Oleh Sychov <sychovoleh@gmail.com>
2017-06-21 22:07:10 +0300

    Add piece with video without font-styles & font styles and video overlaps the header

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', function () {
-  return gulp.src('./src/styles/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 4 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('./dest'))
+gulp.task('sass', function() {
+    return gulp.src('./src/styles/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 7 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./dest'))
 })
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/styles/**/*.scss', ['sass']);
+gulp.task('sass:watch', function() {
+    gulp.watch('./src/styles/**/*.scss', ['sass']);
 });
