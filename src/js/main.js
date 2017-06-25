c935a1792a8a3567eecf7ad22e1829bfb293233b
Oleh Sychov <sychovoleh@gmail.com>
2017-06-25 22:19:39 +0300

    Added jQuery, slick gallery. Ended block of screenshots

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $('.shots__content').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    infinite: false,
+    dots: true,
+    responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    }, {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: 479,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+});
